a2f4cd00c5693640e8b0254bd7479586c668525d
MuraliKrishna <mailstomuralikrishna@gmail.com>
2026-01-18 22:32:32 +0530

    frontend basic created

---
+++ Frontend/perilla-frontend/src/app/employee/list-employee/list-employee.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-list-employee',
+  imports: [],
+  templateUrl: './list-employee.html',
+  styleUrl: './list-employee.css',
+})
+export class ListEmployee {
+
+}
---
+++ Frontend/perilla-frontend/src/app/home/home.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-home',
+  imports: [RouterModule],
+   standalone: true,
+  templateUrl: './home.html',
+  styleUrl: './home.css',
+})
+export class Home {
+
+}
